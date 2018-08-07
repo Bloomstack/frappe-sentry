eacc4ca8b02ee50a9e1525813e4f1d7f2aea445f
Valmik Jangla <mail@valmik.in>
2018-08-07 10:45:54 -0700

    Return if sentry_dsn doesn't exist

--- sentry/utils.py
+++ sentry/utils.py
@@ -4,6 +4,10 @@ from raven import Client
 
 def handle():
     sentry_dsn = frappe.db.get_single_value("Sentry Settings", "sentry_dsn")
+
+    if not sentry_dsn:
+        return
+
     client = Client(sentry_dsn)
     if not frappe.conf.get("developer_mode"):
         client.user_context({
