25279f0dab024cf11abacacb8090c55f99ebecd3
Valmik Jangla <mail@valmik.in>
2018-08-20 05:20:41 -0700

    Wait for frappe ready, and only set user context if not guest

--- sentry/public/js/sentry-web.js
+++ sentry/public/js/sentry-web.js
@@ -1,16 +1,21 @@
-if (!window.sentry_dsn) {
-    frappe.call({
-        method: "sentry.utils.get_sentry_dsn",
-        callback: function (r) {
-            window.localStorage.sentry_dsn = r.message
+frappe.ready(function () {
+    if (!window.sentry_dsn) {
+        frappe.call({
+            method: "sentry.utils.get_sentry_dsn",
+            callback: function (r) {
+                window.localStorage.sentry_dsn = r.message
+            }
+        })
+    }
+
+    if (window.localStorage.sentry_dsn) {
+        Raven.config(window.localStorage.sentry_dsn).install();
+
+
+        if (frappe.sid != "Guest") {
+            Raven.setUserContext({
+                email: frappe.user_id,
+            })
         }
-    })
-}
-
-if (window.localStorage.sentry_dsn) {
-    Raven.config(window.localStorage.sentry_dsn).install();
-
-    Raven.setUserContext({
-        email: frappe.full_name,
-    })
-}
+    }
+});
