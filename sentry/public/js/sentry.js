84ad5acbb4b4edcade725ee0167ccc4c1b73d4f2
Rohan Bansal <rohan@bloomstack.com>
2020-08-24 12:30:45 +0530

    fix: bump sentry to 5.21.1 and delegate to package manager

--- sentry/public/js/sentry.js
+++ sentry/public/js/sentry.js
@@ -1,6 +1,8 @@
-if (frappe.boot.sentry_dsn && !frappe.boot.developer_mode) {
-	Sentry.init({ "dsn": frappe.boot.sentry_dsn });
-	Sentry.configureScope(function (scope) {
-		scope.setUser({ "email": frappe.boot.user.email });
+import { init, configureScope } from '@sentry/browser';
+
+if (frappe.boot.sentry_dsn /* && !frappe.boot.developer_mode */) {
+	init({ dsn: frappe.boot.sentry_dsn });
+	configureScope(function (scope) {
+		scope.setUser({ email: frappe.boot.user.email });
 	});
 }
