2edec21fdaf0712bb2d873183a5e7cacb2092740
Rohan Bansal <rohan@bloomstack.com>
2020-08-28 16:09:52 +0530

    fix: bump sentry to 5.22.0

--- sentry/public/js/sentry-web.js
+++ sentry/public/js/sentry-web.js
@@ -1,22 +1,24 @@
 import { init, configureScope } from '@sentry/browser';
 
-if (!window.sentry_dsn) {
-	frappe.call({
-		method: "sentry.utils.get_sentry_dsn",
-		callback: function (r) {
-			if (r.message) {
-				window.localStorage.sentry_dsn = r.message;
+frappe.ready(function () {
+	if (!window.sentry_dsn) {
+		frappe.call({
+			method: "sentry.utils.get_sentry_dsn",
+			callback: function (r) {
+				if (r.message) {
+					window.localStorage.sentry_dsn = r.message;
+				}
 			}
-		}
-	});
-}
-
-if (window.localStorage.sentry_dsn) {
-	init({ dsn: window.localStorage.sentry_dsn });
-
-	if (frappe.sid != "Guest") {
-		configureScope(function (scope) {
-			scope.setUser({ email: frappe.user_id });
 		});
 	}
-}
+
+	if (window.localStorage.sentry_dsn) {
+		init({ dsn: window.localStorage.sentry_dsn });
+
+		if (frappe.sid != "Guest") {
+			configureScope(function (scope) {
+				scope.setUser({ email: frappe.user_id });
+			});
+		}
+	}
+})
